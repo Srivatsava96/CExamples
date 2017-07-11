d7ade7be98499aee0b7613085eb5f0284457d8df
Srivatsava <srivatsavagummalla@gmail.com>
2017-07-11 20:27:19 +0530

    Linked list with node creation.

--- CppClass/main.cpp
+++ CppClass/main.cpp
@@ -32,6 +32,13 @@ class CNode
     public:
 
     //-------------------------------------------------------------------------
+    CNode(int x)
+    {
+        data.set_data(x);
+        next = NULL;
+    }
+
+    //-------------------------------------------------------------------------
     CNode(CData x)
     {
         data = x;
@@ -107,6 +114,27 @@ class list
     }
 
     //-------------------------------------------------------------------------
+    void append(CNode *tmp)
+    {
+        if (head == NULL)
+        {
+            head = tmp;
+            tail = tmp;
+            return;
+        }
+        else if (tail->get_next() == NULL)
+        {
+            tail->set_next(tmp);
+            tail = tmp;
+            return;
+        }
+        else{
+            cout << "Error"<<endl;
+            exit(0);
+        }
+    }
+
+    //-------------------------------------------------------------------------
     void display()
     {
         if (head == NULL)
@@ -134,28 +162,38 @@ class list
 int main()
 {
     cout << "Hello world!" << endl;
-    list l1;
+    list myList;
     cout << "Select an option"<< endl;
     for(;;)
     {
-        cout << "\n1.Create node, 2.Display linked list 3.Exit"<<endl;
+        cout << "\n1.Create node, 2.Display linked list 3.Create node and then insert in list 4.Exit"<<endl;
         int c,x;
         cin>>c;
         switch (c)
         {
-        case 1:
-            cout<<"Enter the value of node"<<endl;
-            cin>>x;
-            CData d;
-            d.set_data(x);
-            l1.append(d);
-            break;
-        case 2:
-            cout<<"Display"<<endl;
-            l1.display();
-            break;
-        case 3:
-            exit(0);
+            default :
+                cout <<"EXITING--- bye bye"<<endl;
+                exit(0);
+
+            case 1 :
+                cout<<"Enter the value of node"<<endl;
+                cin>>x;
+                CData d;
+                d.set_data(x);
+                myList.append(d);
+                break;
+
+            case 2 :
+                cout<<"Display"<<endl;
+                myList.display();
+                break;
+
+            case 3 :
+                cout<<"Enter the value of node"<<endl;
+                cin>>x;
+                CNode *myNode = new CNode(x);
+                myList.append(myNode);
+                break;
         }
     }
     return 0;
